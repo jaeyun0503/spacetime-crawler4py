7aaf8f23dad8dcd5634c5c723b52f90580745698
Jae Yun Kim <jaeyk14@uci.edu>
2024-04-25 16:33:49 -0700

    Added redirect portion

--- utils/download.py
+++ utils/download.py
@@ -8,7 +8,7 @@ def download(url, config, logger=None):
     host, port = config.cache_server
     resp = requests.get(
         f"http://{host}:{port}/",
-        params=[("q", f"{url}"), ("u", f"{config.user_agent}")])
+        params=[("q", f"{url}"), ("u", f"{config.user_agent}")], allow_redirects=True, max_redirects=5)
 
         # TODO CHECK get method
     try:
